a95b78fc43086249cee8b67d3f6bbc9a4ff9858f
OwnderDuck <upb114@outlook.com>
2025-07-30 19:21:50 +0800

    Add files via upload

--- CILFront.hpp
+++ CILFront.hpp
@@ -11,34 +11,70 @@
 #include <sys/ioctl.h>
 #include <sys/time.h>
 #endif
-
 namespace CILF{
-    char screenBuffer[1145][1145];
-    int FPS=INT_MAX;
+    char screenBuffer[514][514];
+    int FPS=114514;
     int screenWidth=20,screenHeight=10;
     /*
     bool IsKeyPressed(int vk_code){
         return (GetAsyncKeyState(vk_code) & 0x8000)?1:0;
     }
     */
-    void HideCursor() {
+   int GetWidth() {
 #ifdef _WIN32
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        int columns = 80;
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (GetConsoleScreenBufferInfo(hStdout, &csbi)) {
+            columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        }
+        return columns;
+#else
+        struct winsize w;
+        int columns = 80;
+        if(ioctl(0, TIOCGWINSZ, &w) == 0) {
+            columns = w.ws_col;
+        }
+        return columns;
+#endif
+    }
+    int GetHeight() {
+#ifdef _WIN32
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        int rows = 25;
+        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (GetConsoleScreenBufferInfo(hStdout, &csbi)) {
+            rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+        }
+        return rows;
+#else
+        struct winsize w;
+        int rows = 25;
+        if(ioctl(0, TIOCGWINSZ, &w) == 0) {
+            rows = w.ws_row;
+        }
+        return rows;
+#endif
+    }
+    void HideCursor(){
+#ifdef _WIN32
+        HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
         CONSOLE_CURSOR_INFO cursorInfo;
-        GetConsoleCursorInfo(hConsole, &cursorInfo);
-        cursorInfo.bVisible = FALSE;
-        SetConsoleCursorInfo(hConsole, &cursorInfo);
+        GetConsoleCursorInfo(hConsole,&cursorInfo);
+        cursorInfo.bVisible=FALSE;
+        SetConsoleCursorInfo(hConsole,&cursorInfo);
 #else
         std::cout << "\033[?25l" << std::flush;
 #endif
     }
-    void MoveCursor(int x, int y) {
+    void MoveCursor(int x,int y) {
+        
         y<<=1;
 #ifdef _WIN32
 	    // Windows实现
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 		if (hConsole == INVALID_HANDLE_VALUE) {
-			throw std::runtime_error("无法获取控制台句柄");
+            fprintf(stderr, "无法获取控制台句柄\n");
 		}
 		
 		COORD coord;
@@ -46,7 +82,7 @@ namespace CILF{
 		coord.Y = static_cast<SHORT>(y);
 		
 		if (!SetConsoleCursorPosition(hConsole, coord)) {
-			throw std::runtime_error("无法设置光标位置");
+            fprintf(stderr, "无法设置光标位置\n");
 		}
 #else
 		// Linux/macOS实现
@@ -58,8 +94,8 @@ namespace CILF{
         screenWidth=initWindowScreenWidth,screenHeight=initWindowScreenHeight;
         HideCursor();
         MoveCursor(0, 0);
-        for(int i=0;i<114;i++){
-            for(int j=0;j<114;j++){
+        for(int i=0;i<GetHeight();i++){
+            for(int j=0;j<GetWidth();j++){
                 putchar(' ');
             }
 			putchar('\n');
@@ -82,25 +118,7 @@ namespace CILF{
 		}
     }
     void EndDrawing(){
-#ifdef _WIN32
-		// Windows实现
-		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-		if (hConsole == INVALID_HANDLE_VALUE) {
-			throw std::runtime_error("无法获取控制台句柄");
-		}
-		
-		COORD coord;
-		coord.X = static_cast<SHORT>(0);
-		coord.Y = static_cast<SHORT>(0);
-		
-		if (!SetConsoleCursorPosition(hConsole, coord)) {
-			throw std::runtime_error("无法设置光标位置");
-		}
-#else
-		// Linux/macOS实现
-		std::cout << "\033[" << (y + 1) << ";" << (x + 1) << "H";
-		std::cout.flush();
-#endif
+        MoveCursor(0, 0);
         for(int i=0;i<screenHeight-1;i++){
             for(int j=0;j<screenWidth+1;j++){
                 putchar(screenBuffer[j][i]);
@@ -111,6 +129,9 @@ namespace CILF{
             putchar(screenBuffer[j][screenHeight-1]);
         }
     }
+    void DrawPixel(int x, int y, char ch) {
+        screenBuffer[x][y] = ch;
+    };
     void DrawBox(int x1, int y1, int x2, int y2, char boxChar,char fillChar) {
         if(boxChar!='\r'){
             for (int i = x1; i <= x2; i++) {
@@ -147,19 +168,40 @@ namespace CILF{
             }
         }
     }
-    void DrawCircle(int centerX, int centerY, int radius, char circleChar,char fillChar) {
-        if (circleChar != '\r') {
-            for (int y = -radius/2; y <= radius/2; y++) {
-                for (int x = -radius; x <= radius; x++) {
-                    if (x * x + y * y == radius * radius) {
-                        int drawX = centerX + x;
-                        int drawY = centerY + y;
-                        if (drawX >= 0 && drawX < screenWidth && drawY >= 0 && drawY < screenHeight) {
-                            screenBuffer[drawX][drawY] = circleChar;
-                        }
-                    }
+    void DrawCircle(int centerX, int centerY, int radius, char circleChar, char fillChar) {
+        if (circleChar == '\r' && fillChar == '\r') return;
+        int x = 0, y = radius;
+        int d = 1 - radius;
+        while (y >= x) {
+            // 画圆边
+            if (circleChar != '\r') {
+                DrawPixel(centerX + x, centerY + y, circleChar);
+                DrawPixel(centerX - x, centerY + y, circleChar);
+                DrawPixel(centerX + x, centerY - y, circleChar);
+                DrawPixel(centerX - x, centerY - y, circleChar);
+                DrawPixel(centerX + y, centerY + x, circleChar);
+                DrawPixel(centerX - y, centerY + x, circleChar);
+                DrawPixel(centerX + y, centerY - x, circleChar);
+                DrawPixel(centerX - y, centerY - x, circleChar);
+            }
+            // 填充圆内部
+            if (fillChar != '\r') {
+                for (int i = centerX - x + 1; i < centerX + x; ++i) {
+                    DrawPixel(i, centerY + y, fillChar);
+                    DrawPixel(i, centerY - y, fillChar);
+                }
+                for (int i = centerX - y + 1; i < centerX + y; ++i) {
+                    DrawPixel(i, centerY + x, fillChar);
+                    DrawPixel(i, centerY - x, fillChar);
                 }
             }
+            if (d < 0) {
+                d += 2 * x + 3;
+            } else {
+                d += 2 * (x - y) + 5;
+                y--;
+            }
+            x++;
         }
     }
     void DrawText(int x, int y, std::string text){
@@ -169,43 +211,9 @@ namespace CILF{
             }
         }
     }
-    int GetWidth() {
-#ifdef _WIN32
-        CONSOLE_SCREEN_BUFFER_INFO csbi;
-        int columns = 80;
-        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-        if (GetConsoleScreenBufferInfo(hStdout, &csbi)) {
-            columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-        }
-        return columns;
-#else
-        struct winsize w;
-        int columns = 80;
-        if(ioctl(0, TIOCGWINSZ, &w) == 0) {
-            columns = w.ws_col;
-        }
-        return columns;
-#endif
-    }
+    
 
-    int GetHeight() {
-#ifdef _WIN32
-        CONSOLE_SCREEN_BUFFER_INFO csbi;
-        int rows = 25;
-        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-        if (GetConsoleScreenBufferInfo(hStdout, &csbi)) {
-            rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-        }
-        return rows;
-#else
-        struct winsize w;
-        int rows = 25;
-        if(ioctl(0, TIOCGWINSZ, &w) == 0) {
-            rows = w.ws_row;
-        }
-        return rows;
-#endif
-    }
+    
     int GetDelayTime() {
     static long long lastTime = 0;
     long long nowTime = 0;
@@ -219,5 +227,5 @@ namespace CILF{
     int delay = (lastTime == 0) ? 0 : static_cast<int>(nowTime - lastTime);
     lastTime = nowTime;
     return delay;
-}
+    }
 }
